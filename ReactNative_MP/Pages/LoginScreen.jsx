cd4f8208f28b988cddafdbd756d859c2fdde62fa
Miroslav Štěpánek <71689639+mirstep019@users.noreply.github.com>
2023-03-10 15:12:08 +0100

    Login page Deisgn finished

--- ReactNative_MP/Pages/LoginScreen.jsx
+++ ReactNative_MP/Pages/LoginScreen.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import HomeScreen from './HomeScreen';
-import { homeName } from '../components/MainContainer';
 import { useNavigation } from '@react-navigation/native';
+import Logo from '../components/Logo';
 
-function LoginScreen(props) {
+export default function LoginScreen(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigation = useNavigation();
@@ -13,63 +12,143 @@ function LoginScreen(props) {
     props.onLogin()
     navigation.navigate('Home');
   };
-
   return (
-    <Container>
-      <Title>Training Diary</Title>
-      <Input
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <Input
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button onPress={handleLogin}>
-        <ButtonText>Login</ButtonText>
-      </Button>
-    </Container>
+      <Container>
+        <Background>
+          <BigRectangle />
+          <SmallRectangleTop />
+          <SmallRectangleBottom />
+          <Circle />
+        </Background>
+        <ComponentContainer>
+          <NameOfApp>
+            <Logo/>
+            <Title><MainLetter>T</MainLetter>raining <MainLetter>D</MainLetter>iary</Title>
+          </NameOfApp>
+          <Input placeholder="Email" value={email} onChangeText={setEmail}/>
+          <Input placeholder="Password" secureTextEntry value={password} onChangeText={setPassword}/>
+          <Button onPress={handleLogin}>
+            <ButtonText>Login</ButtonText>
+          </Button>
+        </ComponentContainer>
+
+          
+      </Container>
   );
 };
 
-export default LoginScreen;
-
-
 export const Container = styled.View`
-  flex: 1;
+  display: flex;
+  height: 100%;
   align-items: center;
+`;
+export const NameOfApp = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: top;
   justify-content: center;
-  background-color: #fff;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const ComponentContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+export const MainLetter = styled.Text`
+  color: #5200FF;
+`;
+
+export const Background = styled.View`
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.26);
+  overflow: hidden;
+`;
+
+export const BigRectangle = styled.View`
+  position: absolute;
+  top: -50px;
+  left: -50px;
+  width: 250px;
+  height: 250px;
+  background-color: #5200FF;
+  opacity: 0.3;
+  transform: rotate(-20deg);
+  border-radius: 40px;
+`;
+
+export const SmallRectangleTop = styled.View`
+  position: absolute;
+  top: 50px;
+  right: -10px;
+  width: 100px;
+  height: 100px;
+  background-color: rgba(124, 124, 124, 0.85);
+  transform: rotate(-20deg);
+  border-radius: 20px;
+`;
+
+export const SmallRectangleBottom = styled.View`
+  position: absolute;
+  bottom: 70px;
+  left: 30px;
+  width: 100px;
+  height: 100px;
+  background-color: rgba(124, 124, 124, 0.85);
+  transform: rotate(35deg);
+  border-radius: 20px;
+`;
+
+export const Circle = styled.View`
+  position: absolute;
+  bottom: -200px;
+  right: -170px;
+  width: 400px;
+  height: 400px;
+  background-color: #5200FF;
+  opacity: 0.3;
+  border-radius: 1000%;
 `;
 
 export const Title = styled.Text`
-  font-size: 35px;
-  font-weight: 800;
+  font-size: 35px; 
+  font-weight: 800; 
   margin-bottom: 30px;
+  margin-bottom: 30px;
+  color: #fff;
+  margin-left: 5px;
 `;
 
 export const Input = styled.TextInput`
-  width: 80%;
-  height: 7%;
-  border-width: 2px;
-  border-color: #5c16c4;
-  border-radius: 15px;
-  margin-bottom: 20px;
-  padding: 15px;
+  width: 80%; 
+  height: 55px; 
+  border-width: 2px; 
+  border-color: #5c16c4; 
+  border-radius: 15px; 
+  padding: 15px; 
   font-size: 18px;
+  margin-bottom: 20px;
+  background-color: #EAE2FF;
 `;
 
 export const Button = styled.TouchableOpacity`
-  background-color: #5c16c4;
-  padding: 10px;
-  border-radius: 15px;
-  width: 80%;
-  height: 7%;
-  display: flex;
+  background-color: #5c16c4; 
+  padding: 10px; 
+  border-radius: 15px; 
+  width: 80%; 
+  height: 60px; 
+  display: flex; 
   justify-content: center;
+  margin-top: 30px;
 `;
 
 export const ButtonText = styled.Text`
@@ -77,7 +156,6 @@ export const ButtonText = styled.Text`
   font-size: 18px;
   font-weight: bold;
   text-align: center;
-
 `;
 
 //#8c43ff
